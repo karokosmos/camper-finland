de625bfa2ac684a3b92ed167740a54d2bc4d2c02
Karoliina Kosmos <karokosmos@hotmail.com>
2026-06-11 17:05:53 +0300

    use dynamic paths for articles

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
+const article = defineCollection({
+  loader: glob({ pattern: '**/*.mdx', base: './src/articles' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    slug: z.string(),
+    published: z.date(),
+    updated: z.nullish(z.date()),
+    featured: z.boolean()
+  }),
+});
+
+export const collections = { article };
